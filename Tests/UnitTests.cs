cef4e95c37d882a8182826ec2f8b7956105e29f6
Vasavi Kotipalli <vkotipalli2@huskers.unl.edu>
2022-04-04 18:24:37 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -10,7 +10,7 @@ namespace GithubActionsLab
         public void Add_Valid()
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
-            Assert.AreEqual(5, Program.Add("3", "7"));
+            Assert.AreEqual(5, Program.Add("3", "2"));
             Assert.AreEqual(12, Program.Add("5", "7"));
         }
 
